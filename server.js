2fe2defa33f3f6f34d9779612a715fee73f7a33b
Piotr Sawicki <01143606@pw.edu.pl>
2021-01-14 17:27:00 +0100

    added option to reserve or rent a car

--- server.js
+++ server.js
@@ -20,12 +20,9 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.post('/api/world', (req, res) => {
-  console.log(req.body);
-  res.send(
-    `I received your POST request. This is what you sent me: ${req.body.post}`,
-  );
-});
+function formatDate(name) {
+    return 'TO_CHAR("' + name + '", \'YYYY-MM-DD\') AS "' + name + '"';
+}
 
 app.get('/api/brands', (req, res) => {
     client.query('SELECT "ID" FROM "CAR_BRAND";', (err, qres) => {
@@ -69,8 +66,8 @@ app.get('/api/cars', (req, res) => {
 });
 
 app.get('/api/available_cars', (req, res) => {
-    client.query('SELECT "PRODUCTION_YEAR", "CAR_MODEL_ID", "CAR_BRAND_ID", "CAR_CLASS_ID" FROM "CAR" ' +
-        'JOIN "CAR_MODEL" ON "CAR"."CAR_MODEL_ID" = "CAR_MODEL"."ID" WHERE "CAR"."ID" NOT IN (SELECT "CAR_ID" FROM "CAR_STATUS");', (err, qres) => {
+    client.query('SELECT "CAR"."ID", "PRODUCTION_YEAR", "CAR_MODEL_ID", "CAR_BRAND_ID", "CAR_CLASS_ID" FROM "CAR" ' +
+        'JOIN "CAR_MODEL" ON "CAR"."CAR_MODEL_ID" = "CAR_MODEL"."ID" WHERE "RESERVATION_ID" ISNULL OR "RESERVATION_ID" NOT IN (SELECT "ID" FROM "RESERVATION");', (err, qres) => {
         if (err) {
             res.send({message: []});
             return;
@@ -133,83 +130,103 @@ app.get('/api/roles', (req, res) => {
     });
 });
 
+async function isEmailRegistered(email) {
+    const rows = await client.query('SELECT "ID" FROM "EMPLOYEE" UNION SELECT "ID" FROM "CUSTOMER";');
+    return rows.length !== 0;
+}
+
 app.post('/api/add_employee', (req, res) => {
     const employee = req.body;
-    const address = 'INSERT INTO "ADDRESS" ("ADDRESS1", "ADDRESS2", "POSTAL_CODE", "CITY") ' + 
-        insertValuesString([employee.address1, employee.address2, employee.postalCode, employee.city]);
-    console.log(address);
-    client.query(address, (err, qres) => {
-        if (err) {
-            res.send({message: 'Error'});
-            console.log(err);
-            return;
-        }
-        client.query('SELECT "ID" FROM "ADDRESS" WHERE "ID"=(SELECT MAX("ID") FROM "ADDRESS");', (err, qres) => {
-            if (err) {
-                res.send({message: 'Error'});
-                console.log(err);
-                return;
-            }
-            const addressID = qres.rows[0]["ID"];
-            const q = 'INSERT INTO "EMPLOYEE" ("ID", "PASSWORD", "FIRST_NAME", "LAST_NAME", "PESEL", "SALARY", "ACCOUNT_NO", "ADDRESS_ID") ' +
-                insertValuesString([employee.email, employee.password, employee.firstName, employee.lastName, 
-                    employee.PESEL, employee.salary, employee.accountNumber, addressID]);
-            console.log(q);
-            client.query(q, (err, qres) => {
+    isEmailRegistered(employee.email).then((isRegistered) => {
+        if (!isRegistered) {
+            const address = 'INSERT INTO "ADDRESS" ("ADDRESS1", "ADDRESS2", "POSTAL_CODE", "CITY") ' + 
+                insertValuesString([employee.address1, employee.address2, employee.postalCode, employee.city]);
+            console.log(address);
+            client.query(address, (err, qres) => {
                 if (err) {
                     res.send({message: 'Error'});
                     console.log(err);
                     return;
                 }
-                client.query('INSERT INTO "EMPLOYEE_ROLE" ("ROLE_ID", "EMPLOYEE_ID") ' + insertValuesString([employee.role.id, employee.email]), (err, qres) => {
+                client.query('SELECT "ID" FROM "ADDRESS" WHERE "ID"=(SELECT MAX("ID") FROM "ADDRESS");', (err, qres) => {
                     if (err) {
                         res.send({message: 'Error'});
                         console.log(err);
                         return;
                     }
-                    res.send({message: 'Success'});
-                })
+                    const addressID = qres.rows[0]["ID"];
+                    const q = 'INSERT INTO "EMPLOYEE" ("ID", "PASSWORD", "FIRST_NAME", "LAST_NAME", "PESEL", "SALARY", "ACCOUNT_NO", "ADDRESS_ID") ' +
+                        insertValuesString([employee.email, employee.password, employee.firstName, employee.lastName, 
+                            employee.PESEL, employee.salary, employee.accountNumber, addressID]);
+                    console.log(q);
+                    client.query(q, (err, qres) => {
+                        if (err) {
+                            res.send({message: 'Error'});
+                            console.log(err);
+                            return;
+                        }
+                        client.query('INSERT INTO "EMPLOYEE_ROLE" ("ROLE_ID", "EMPLOYEE_ID") ' + insertValuesString([employee.role.id, employee.email]), (err, qres) => {
+                            if (err) {
+                                res.send({message: 'Error'});
+                                console.log(err);
+                                return;
+                            }
+                            res.send({message: 'Success'});
+                        })
+                    });
+        
+                });
             });
-
-        });
-    });
+        } else {
+            res.send({mesage: 'Error'});
+        }
+    })
 
 });
 
+function date() {
+    const date = new Date();
+    return date.getFullYear() + '-' + String(date.getMonth() + 1).padStart(2, '0') + '-' + String(date.getDate()).padStart(2, '0');
+}
+
 app.post('/api/add_customer', (req, res) => {
     const customer = req.body;
-    const address = 'INSERT INTO "ADDRESS" ("ADDRESS1", "ADDRESS2", "POSTAL_CODE", "CITY") ' + 
-        insertValuesString([customer.address1, customer.address2, customer.postalCode, customer.city]);
-    console.log(address);
-    client.query(address, (err, qres) => {
-        if (err) {
-            res.send({message: 'Error'});
-            console.log(err);
-            return;
-        }
-        client.query('SELECT "ID" FROM "ADDRESS" WHERE "ID"=(SELECT MAX("ID") FROM "ADDRESS");', (err, qres) => {
-            if (err) {
-                res.send({message: 'Error'});
-                console.log(err);
-                return;
-            }
-            const addressID = qres.rows[0]["ID"];
-            const now = new Date();
-            const date = now.getFullYear() + '-' + String(now.getMonth() + 1).padStart(2, '0') + '-' + String(now.getDate()).padStart(2, '0');
-            const q = 'INSERT INTO "CUSTOMER" ("ID", "PASSWORD", "FIRST_NAME", "LAST_NAME", "PESEL", "LICENSE_NO", "REGISTEREDAT", "ADDRESS_ID") ' +
-                insertValuesString([customer.email, customer.password, customer.firstName, customer.lastName, 
-                    customer.PESEL, customer.licenseNumber, date, addressID]);
-            console.log(q);
-            client.query(q, (err, qres) => {
+    isEmailRegistered(customer.email).then((isRegistered) => {
+        if (!isRegistered) {
+            const address = 'INSERT INTO "ADDRESS" ("ADDRESS1", "ADDRESS2", "POSTAL_CODE", "CITY") ' + 
+                insertValuesString([customer.address1, customer.address2, customer.postalCode, customer.city]);
+            console.log(address);
+            client.query(address, (err, qres) => {
                 if (err) {
                     res.send({message: 'Error'});
                     console.log(err);
                     return;
                 }
-                res.send({message: 'Success'});
+                client.query('SELECT "ID" FROM "ADDRESS" WHERE "ID"=(SELECT MAX("ID") FROM "ADDRESS");', (err, qres) => {
+                    if (err) {
+                        res.send({message: 'Error'});
+                        console.log(err);
+                        return;
+                    }
+                    const addressID = qres.rows[0]["ID"];
+                    const q = 'INSERT INTO "CUSTOMER" ("ID", "PASSWORD", "FIRST_NAME", "LAST_NAME", "PESEL", "LICENSE_NO", "REGISTEREDAT", "ADDRESS_ID") ' +
+                        insertValuesString([customer.email, customer.password, customer.firstName, customer.lastName, 
+                            customer.PESEL, customer.licenseNumber, date(), addressID]);
+                    console.log(q);
+                    client.query(q, (err, qres) => {
+                        if (err) {
+                            res.send({message: 'Error'});
+                            console.log(err);
+                            return;
+                        }
+                        res.send({message: 'Success'});
+                    });
+                });
             });
-        });
-    });
+        } else {
+            res.send({mesage: 'Error'});
+        }
+    })
 });
 
 app.get('/api/classes', (req, res) => {
@@ -222,5 +239,181 @@ app.get('/api/classes', (req, res) => {
         res.send({message: qres.rows});
     });
 });
+
+app.post('/api/login', (req, res) => {
+    const info = req.body;
+    client.query('SELECT "ID" FROM "CUSTOMER" WHERE "ID" = \'' + info.email + '\' AND "PASSWORD" = \'' + info.password + '\';', (err, qres) => {
+        if (err) {
+            res.send({message: null});
+            console.log(err);
+            return;
+        }
+        if (qres.rows.length === 1) {
+            res.send({message: {
+                email: qres.rows[0]['ID'],
+                type: 'customer',
+            }});
+        } else {
+            client.query('SELECT "ID" FROM "EMPLOYEE" WHERE "ID" = \'' + info.email + '\' AND "PASSWORD" = \'' + info.password + '\';', (err, qres) => {
+                if (err) {
+                    res.send({message: null});
+                    console.log(err);
+                    return;
+                }
+                if (qres.rows.length === 1) {
+                    const id = qres.rows[0]['ID'];
+                    client.query('SELECT * FROM "EMPLOYEE_ROLE" WHERE "EMPLOYEE_ID" = \'' + id + '\' AND ' + 
+                    '"ROLE_ID" IN (SELECT "ID" FROM "ROLE" WHERE "NAME" = \'Administrator\');', (err, qres) => {
+                        if (err) {
+                            res.send({message: null});
+                            console.log(err);
+                            return;
+                        }
+                        if (qres.rows.length > 0) {
+                            res.send({message: {
+                                email: id,
+                                type: 'admin',
+                            }});
+                        } else {
+                            res.send({message: {
+                                email: id,
+                                type: 'worker',
+                            }});
+                        }
+                    });
+                } else {
+                    res.send({message: null});
+                }
+            });
+        }
+    });
+});
+
+app.post('/api/add_reservation', (req, res) => {
+    const reservation = req.body;
+    client.query('INSERT INTO "RESERVATION" ("CREATED_AT", "START_DATE", "END_DATE", "PRICE", "EXPIRES", "CUSTOMER_ID") ' +
+        insertValuesString([date(), reservation.startDate, reservation.endDate, reservation.price, reservation.endDate, reservation.customerID]), (err, qres) => {
+            if (err) {
+                res.send({message: 'Error'});
+                console.log(err);
+                return;
+            }
+            client.query('SELECT MAX("ID") FROM "RESERVATION";', (err, qres) => {
+                if (err || qres.rows.length === 0) {
+                    res.send({message: 'Error'});
+                    console.log(err);
+                    return;
+                }
+                const reservationID = qres.rows[0]["max"];
+                const q = 'UPDATE "CAR" SET "RESERVATION_ID" = \'' + reservationID + '\' WHERE "ID" = \'' + reservation.carID + '\';'
+                console.log(q);
+                client.query(q, (err, qres) => {
+                    if (err) {
+                        res.send({message: 'Error'});
+                        console.log(err);
+                        return;
+                    }
+                        res.send({message: 'Success'});
+                });
+            });
+        });
+});
+
+app.post('/api/add_rental', (req, res) => {
+    const reservationID = req.body.ID;
+    const rentalStart = req.body.startDate;
+    const q = 'SELECT ' + formatDate("END_DATE") + ', "PRICE", "CUSTOMER_ID" FROM "RESERVATION" WHERE "ID" = \'' + reservationID + '\';';
+    console.log(q);
+    client.query(q, (err, qres) => {
+        if (err || qres.length === 0) {
+            res.send({message: 'Error'});
+            console.log(err);
+            return;
+        }
+        const reservation = qres.rows[0];
+        const q = 'INSERT INTO "RENTAL" ("CREATED_AT", "START_DATE", "END_DATE", "PRICE", "CUSTOMER_ID") ' + 
+        insertValuesString([date(), rentalStart, reservation['END_DATE'], reservation['PRICE'], reservation['CUSTOMER_ID']]);
+        console.log(q);
+        client.query(q, (err, qres) => {
+                if (err) {
+                    res.send({message: 'Error'});
+                    console.log(err);
+                    return;
+                }
+                client.query('SELECT MAX("ID") FROM "RENTAL";', (err, qres) => {
+                    if (err || qres.rows.length === 0) {
+                        res.send({message: 'Error'});
+                        console.log(err);
+                        return;
+                    }
+                    const rentalID = qres.rows[0]['max'];
+                    client.query('UPDATE "CAR" SET "RENTAL_ID" = \'' + rentalID + '\' WHERE "RESERVATION_ID" = \'' + reservationID + '\';', (err, qres) => {
+                        if (err) {
+                            res.send({message: 'Error'});
+                            console.log(err);
+                            return;
+                        }
+                            res.send({message: 'Success'});
+                    });
+                });
+            });
+    });
+});
+
+app.get('/api/reservations/:id', (req, res) => {
+    const q = 'SELECT "CAR_MODEL_ID", "CAR_BRAND_ID", "RESERVATION"."ID", ' + formatDate("START_DATE") + ', ' + 
+    formatDate("END_DATE") + ', "PRICE" FROM "CAR" ' +
+    'JOIN "CAR_MODEL" ON "CAR"."CAR_MODEL_ID" = "CAR_MODEL"."ID" ' + 
+    'JOIN "RESERVATION" ON "CAR"."RESERVATION_ID" = "RESERVATION"."ID" ' + 
+    'WHERE "CUSTOMER_ID" = \'' + req.params['id'] + '\' AND "RENTAL_ID" ISNULL;';
+    console.log(q);
+    client.query(q, (err, qres) => {
+            if (err) {
+                res.send({message: null});
+                console.log(err);
+                return;
+            }
+            res.send ({message: qres.rows});
+        });
+});
+
+app.get('/api/rentals/:id', (req, res) => {
+    client.query('SELECT "CAR_MODEL_ID", "CAR_BRAND_ID", "RENTAL"."ID", ' + formatDate("START_DATE") + ', ' + 
+    formatDate("END_DATE") + ', "PRICE" FROM "CAR" ' +
+        'JOIN "CAR_MODEL" ON "CAR"."CAR_MODEL_ID" = "CAR_MODEL"."ID" ' + 
+        'JOIN "RENTAL" ON "CAR"."RENTAL_ID" = "RENTAL"."ID" ' + 
+        'WHERE "CUSTOMER_ID" = \'' + req.params['id'] + '\';', (err, qres) => {
+            if (err) {
+                res.send({message: null});
+                console.log(err);
+                return;
+            }
+            res.send({message: qres.rows});
+        });
+});
+
+app.post('/api/end_rental', (req, res) => {
+    client.query('UPDATE "RENTAL" SET "END_DATE" = \'' + req.body.endDate + '\' WHERE "ID" = \'' + req.body.ID + '\';', (err, qres) => {
+        if (err) {
+            res.send({message: 'Error'});
+            console.log(err);
+            return;
+        }
+        res.send({message: 'Success'});
+    });
+});
+
+app.post('/api/cancel_reservation', (req, res) => {
+    client.query('DELETE FROM "RESERVATION" WHERE "ID" = \'' + req.body.ID + '\';'), (err, qres) => {
+        if (err) {
+            res.send({message: 'Error'});
+            console.log(err);
+            return;
+        }
+        res.send({message: 'Success'});
+    }
+});
+
+
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
